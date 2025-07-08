73b6eba4bd98935450fd86bf6a3b23c16944478e
OPARKAS3 <98760822+OPARKAS3@users.noreply.github.com>
2025-07-08 22:11:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.post("/signal")
+@app.post("/signal/")
 async def receive_signal(request: Request):
     data = await request.json()
     print("Received:", data)
